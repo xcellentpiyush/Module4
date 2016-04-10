4d1d963f3d78b52007ba4c9badbb38dd86f93319
xcellentpiyush <xcellent.piyush@gmail.com>
2016-04-10 22:23:52 +0530

    My Pages

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -82,11 +82,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl, 
+  homeHtmlUrl, 
   function(responseText){
   	document.querySelector("main-content").innerHTML = responseText;
   }, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitely setting the flag to get JSON from server processed into an object literal
+  false); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
